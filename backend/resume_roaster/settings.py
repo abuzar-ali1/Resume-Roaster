9068fc0ca42e42bbedbc7629b7169dbf4a5480bb
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-04-27 13:37:55 +0500

    Production Made

--- backend/resume_roaster/settings.py
+++ backend/resume_roaster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,6 +31,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'my-portfolio-k9q5.onrender.com,localhost,127.0.0.1').split(',')
+
+
 CORS_ALLOW_ALL_ORIGINS = True
 # Application definition
 
@@ -46,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
